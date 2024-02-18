cf95673135e85b36038ba7603b54ac473b4f625a
Seungil <seungilbang@khu.ac.kr>
2024-02-18 17:07:31 +0900

    user-service 컨트롤러

---
+++ src/main/java/com/example/userservice/UserController.java
@@ -0,0 +1,34 @@
+package com.example.userservice;
+
+import com.example.userservice.vo.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @Autowired // 권장하지 않는 DI 방법
+    private Greeting greeting;
+
+    Environment env;
+
+    public UserController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+//        return env.getProperty("greeting.message");
+        return greeting.getMessage();
+    }
+
+}
